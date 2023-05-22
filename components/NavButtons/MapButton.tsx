05fc587cdf84809bf384c82ada3e00da7295c88c
Ryan <rcheng888446@gmail.com>
2023-05-22 16:08:51 -0700

    applied newest button changes to all buttons

--- components/NavButtons/MapButton.tsx
+++ components/NavButtons/MapButton.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
-import MapIcon from "./NavIcons/MapIcon";
 
 const MapButton = () => {
+    const flag = useRouter().pathname === "/map";
+    const but_f = flag ?  "bg-icon-dark" : "bg-white";
+    const icon_f = flag ? "white" : "#33363F";
+    
     return (
-        <Link href="/Map">
-            <div className="rounded-full bg-black h-50px w-50px">
-                <MapIcon width="35" height="35" fill="white"/>
+        <Link href="/map">
+            <div className={`rounded-full w-[50px] h-[50px] ${but_f} flex justify-center items-center transition-all duration-200 hover:scale-110`}>
+            <svg width="40" height="40" stroke={icon_f} fill="none" viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30.625 24.487V7.95632C30.625 7.63796 30.3035 7.42027 30.0079 7.53851L23.4684 10.1543C23.3806 10.1894 23.2839 10.196 23.1922 10.173L11.8078 7.32695C11.7161 7.30401 11.6194 7.31057 11.5316 7.3457L4.65787 10.0952C4.48703 10.1635 4.375 10.329 4.375 10.513V27.0437C4.375 27.362 4.69654 27.5797 4.99213 27.4615L11.5316 24.8457C11.6194 24.8106 11.7161 24.804 11.8078 24.8269L23.1922 27.673C23.2839 27.696 23.3806 27.6894 23.4684 27.6543L30.3421 24.9048C30.513 24.8365 30.625 24.671 30.625 24.487Z" stroke-width="2" stroke-linejoin="round"/>
+                <path d="M23.3333 27.7083V10.2083" stroke-width="2"/>
+                <path d="M11.6667 24.7917L11.6667 7.29166" stroke-width="2"/>
+            </svg>
             </div>
         </Link>
     )
--- components/NavButtons/SearchButton.tsx
+++ components/NavButtons/SearchButton.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
-import SearchIcon from "./NavIcons/SearchIcon";
 
 const SearchButton = () => {
+    const flag = useRouter().pathname === "/search";
+    const but_f = flag ?  "bg-icon-dark" : "bg-white";
+    const icon_f = flag ? "white" : "#33363F";
+    
     return (
-        <Link href="/Search">
-            <div className="rounded-full bg-black h-50px w-50px">
-                <SearchIcon width="35" height="35" fill="white"/>
+        <Link href="/search">
+            <div className={`rounded-full w-[50px] h-[50px] ${but_f} flex justify-center items-center transition-all duration-200 hover:scale-110`}>
+                <svg width="40" height="40" stroke={icon_f} fill="none" viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="16.0417" cy="16.0417" r="10.2083" stroke-width="2"/>
+                    <path d="M16.0416 11.6667C15.4671 11.6667 14.8982 11.7798 14.3674 11.9997C13.8366 12.2195 13.3543 12.5418 12.948 12.9481C12.5418 13.3543 12.2195 13.8366 11.9997 14.3674C11.7798 14.8982 11.6666 15.4671 11.6666 16.0417" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M29.1666 29.1667L24.7916 24.7917" stroke-width="2" stroke-linecap="round"/>
+                </svg>
             </div>
         </Link>
     )
