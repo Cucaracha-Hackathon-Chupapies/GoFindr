568abc03fe18575f03ea06f30dcd6b747fecd0a0
Jason Louie <jason.jw.louie@gmail.com>
2023-05-31 22:58:53 -0700

    basic account page template added

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProfileBG from "@/components/Backgrounds/ProfileBG";
+import { Account } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const Profile = () => {
+
+    const [userData, setUserData] = useState<Account>()
+
+    const router = useRouter()
+
+    useEffect(() => {
+        axios.post('/api/get/account', {id: localStorage.getItem('id')})
+        .then((res) => {
+
+            setUserData(res.data)
+
+        }).catch(() => router.push('/login'))
+    }, [])
+
     return (
         <div>
             <ProfileBG />
-            profiling omg
+            <div>
+
+                <img src={userData?.icon}/>
+
+                <h1>{userData?.username}</h1>
+
+                <button>Edit Profile</button>
+
+
+
+            </div>
         </div>
     )
 }
