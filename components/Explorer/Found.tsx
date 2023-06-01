7f5483fe984661cc31f874538c10d2273c9a6fca
Jason Louie <jason.jw.louie@gmail.com>
2023-05-31 20:19:09 -0700

    added review section and save button to found page

--- components/Explorer/Found.tsx
+++ components/Explorer/Found.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import Item from "./Item";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import { StoreRating } from "@prisma/client";
 
 const Found = () => {
     /*
@@ -21,18 +22,40 @@ const Found = () => {
 
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
+    const [rated, setRated] = useState(false)
+    const [rating, setRating] = useState(3)
+    const [comment, setComment] = useState("")
+    const [ratings, setRatings] = useState<StoreRating[]>()
     const [items, setItems] = useState<any>([]) //HAHAHAHAAH LOLLLLL FUNNY LMFAAO any :))))
     const fill = "#ffffff";
     const fill2 = "#D9D9D9";
 
     useEffect(() => {
-        axios.post('/api/getshop', {name: storeName})
+        axios.post('/api/get/shop', {name: storeName})
         .then((res) => {
             setName(res.data.displayName)
             setDescription(res.data.description)
             setItems(res.data.items)
+            setRated(res.data.rated)
+        })
+
+        axios.post('/api/get/ratings', {storeName: storeName})
+        .then((res) => {
+            setRatings(res.data)
         })
     }, [])
+
+    const createReview = (e: any) => {
+        e.preventDefault()
+
+        if (!localStorage.getItem('id') || !rating) return
+
+        axios.post('/api/create/rating', {id: localStorage.getItem('id'), storeName: storeName, rating: rating, comment: comment})
+    }
+
+    const saveShop = () => {
+        axios.post('/api/account/save', {id: localStorage.getItem('id') || "", name: storeName})
+    }
 
     return (
         <div className="relative z-999 ">
@@ -69,6 +92,31 @@ const Found = () => {
                     {items.map((item: any) => (
                         item.popular && <Item id={item.id} name={item.displayName} price={item.price} img={'/placeholder.jpg'} fill={fill} key={item.id}/>
                     ))}
+                </div>
+
+                {/* The Following HTML Shows the Reviews the Store Has */}
+                <div>
+                    <h1>Reviews</h1>
+                    {ratings?.map((rating) => (
+                        <div>
+                            <h1>{rating.rating}</h1>
+                            <h3>{rating.comment}</h3>            
+                            <p>{new Date(rating.created).toDateString()}</p>           
+                        </div>
+                    ))}
+                </div>
+                
+                {/* The Following HTML Shows a Button Used to Save/Pin The Store To the Users Account */}
+                <button onClick={saveShop}>Save</button>
+
+                {/* The Following HTML Shows a Form To Write A Review For the Store Given That The User Hasn't Already Written One */}
+                <div>
+                    <h2>Write A Review</h2>
+                    <form onSubmit={createReview}>
+                        <input type="range" min={1} max={5} value={rating} onChange={(e) => setRating(parseInt(e.target.value))}/>
+                        <textarea value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                        <button type="submit" disabled={rated ? true : false}>Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
