4b9878a1d353fb0a3b1268c0b916176e4344d57d
Ryan <rcheng888446@gmail.com>
2023-06-01 23:12:11 -0700

    edited stroke properties to match react

--- components/ImageUpload/Uploader.tsx
+++ components/ImageUpload/Uploader.tsx
@@ -2,11 +2,12 @@ import axios from "axios"
 import { Dispatch, SetStateAction, useCallback } from "react"
 
 interface Props {
+    message: string;
     setState: Dispatch<SetStateAction<string | undefined>>,
     setUploadError?: Dispatch<SetStateAction<boolean>>
 }
 
-const Uploader = ({setState, setUploadError}: Props) => {
+const Uploader = ({message, setState, setUploadError}: Props) => {
     const handleUpload = useCallback(async (e: any) => {
         let file = e.target.files[0]
 
@@ -39,7 +40,7 @@ const Uploader = ({setState, setUploadError}: Props) => {
 
         <div className="flex h-[55px] w-[330px] lg:h-[50px] bg-white rounded italic pl-4 mt-8 lg:mt-4 items-center bg-gray-200">
             <label htmlFor="uploadInput" className="flex items-center justify-center w-full h-full cursor-pointer ">
-                Upload image of store
+                {message}
                 <input type="file" id="uploadInput" accept="image/*" onChange={(e) => handleUpload(e)} className="hidden"/>
             </label>          
         </div>
--- components/NavBar/NavButtons/CreateButton.tsx
+++ components/NavBar/NavButtons/CreateButton.tsx
@@ -11,9 +11,9 @@ const CreateButton = () => {
         <Link href="/create">
             <div className={`rounded-full w-[50px] h-[50px] ${but_f} flex justify-center items-center transition-all duration-200 hover:scale-110`}>
             <svg width="35" height="35" viewBox="0 0 35 35" stroke={icon_f} fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-[1px] mb-[1px]">
-                <circle cx="17.5" cy="17.5" r="13.125" stroke-width="2"/>
-                <path d="M17.5 21.875L17.5 13.125" stroke-width="2" stroke-linecap="square"/>
-                <path d="M21.875 17.5L13.125 17.5" stroke-width="2" stroke-linecap="square"/>
+                <circle cx="17.5" cy="17.5" r="13.125" strokeWidth="2"/>
+                <path d="M17.5 21.875L17.5 13.125" strokeWidth="2" strokeLinecap="square"/>
+                <path d="M21.875 17.5L13.125 17.5" strokeWidth="2" strokeLinecap="square"/>
             </svg>
             </div>
         </Link>
